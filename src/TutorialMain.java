76c0e14bcd91a84d816e597996059721c6f8ee21
“Lakith <lakith.rambukkanage@avtra.com>
2020-10-20 19:46:24 +0530

    annotations,pk and colletions
    @embeddable
    @elemwntCollection

--- src/TutorialMain.java
+++ src/TutorialMain.java
@@ -1,2 +1,71 @@
-package PACKAGE_NAME;public class TutorialMain {
+import java.util.Date;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.iota.models.dto.Address;
+import org.iota.models.dto.UserDetails;
+
+public class TutorialMain {
+	public static void main(String[] args) {
+
+		UserDetails userDetails = new UserDetails();
+
+		//userDetails.setUserID(1);
+		userDetails.setUserName("Lakith Ram");
+		userDetails.setJoinedDate(new Date());
+		userDetails.setDescription("description");
+
+
+		Address address1 = new Address();
+		address1.setCity("col1");
+		address1.setState("Western1");
+		address1.setStreet("red street1");
+		address1.setZipcode("1111");
+
+		Address address2 = new Address();
+		address2.setCity("col2");
+		address2.setState("Western2");
+		address2.setStreet("red street2");
+		address2.setZipcode("2222");
+
+		Address address3 = new Address();
+		address3.setCity("col3");
+		address3.setState("Western3");
+		address3.setStreet("red street3");
+		address3.setZipcode("3333");
+
+
+		userDetails.getAddresses().add(address1);
+		userDetails.getAddresses().add(address2);
+		userDetails.getAddresses().add(address3);
+
+
+
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		Session session = sessionFactory.openSession();
+
+		session.beginTransaction();
+		session.save(userDetails);
+		session.getTransaction().commit();
+
+		session.close();
+
+		userDetails = null;
+
+		session = sessionFactory.openSession();
+		session.beginTransaction();
+
+		userDetails = session.get(UserDetails.class,1);
+		for (Address a : userDetails.getAddresses()) {
+			System.out.println(a.getZipcode());
+		}
+		System.out.println("done");
+		session.getTransaction().commit();
+		System.out.println("done");
+		session.close();
+
+	}
+
+
 }
--- src/org/iota/models/dto/UserDetails.java
+++ src/org/iota/models/dto/UserDetails.java
@@ -1,11 +1,52 @@
-package org.iota.models;
+package org.iota.models.dto;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
-@Entity
+import org.hibernate.annotations.CollectionId;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+
+@Entity(name = "USER_DETAILS")
 public class UserDetails {
+	@Id	@GeneratedValue
 	private int userID;
 	private String userName;
+	@Temporal(TemporalType.DATE)
+	private Date joinedDate;
+	private String description;
+	@ElementCollection
+	@JoinTable( name = "USER_ADDRESSES", joinColumns =@JoinColumn(name = "userID"))
+	@GenericGenerator(name = "sequence_gen",strategy = "sequence")
+	@CollectionId(columns ={@Column(name = "ADDRESS_ID")},generator ="sequence_gen",type =@Type(type = "long" ))
+	private Collection<Address> addresses = new ArrayList<Address>();
+
+	public Collection<Address> getAddresses() {return addresses;	}
+
+	public void setAddresses(Collection<Address> addresses) {this.addresses= addresses;	}
+
+
+	public String getDescription() {return description;	}
+
+	public void setDescription(String description) {this.description = description; }
+
+	public Date getJoinedDate() {return joinedDate;	}
+
+	public void setJoinedDate(Date joinedDate) {this.joinedDate = joinedDate;}
 
 	public String getUserName() {
 		return userName;
